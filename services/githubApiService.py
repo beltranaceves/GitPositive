9d62ccad26db152333a2a129a9b6be5135a75769
Beltrán Aceves <beltranaceves@gmail.com>
2022-05-12 00:46:25 +0000

    Massive changes, Sentiment analysis and github login done

--- services/githubApiService.py
+++ services/githubApiService.py
@@ -1,10 +1,25 @@
 import requests
+
 def getRepositoriesByUsername(username):
     url = f'https://api.github.com/users/{username}/repos'
     response = requests.get(url)
     return response.json()
 
-def getCommitsByRepositoryUrlAndYear(repoUrl) :
+def getCommitsByRepositoryUrl(repoUrl) :
     url = f'{repoUrl}/commits'
     response = requests.get(url)
     return response.json()
+
+def getCommitsByUsernameAndYear(username, year):
+    repositories = getRepositoriesByUsername(username)
+    commits = []
+    print(repositories)
+    for repository in repositories:
+      repositoryCommits = getCommitsByRepositoryUrl(repository["url"])
+      #Filter them by year
+      for repositoryCommit in repositoryCommits:
+        commitDate = repositoryCommit["commit"]["author"]["date"]
+        commitYear = commitDate.split("-")[0]
+        if commitYear == year:  
+          commits.append(repositoryCommit)
+    return commits
